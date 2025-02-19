ace72b7742174b849b15148a9b9f550db19ab947
JK Global IT Solution <163848085+JKGlobalItSolutions@users.noreply.github.com>
2025-02-19 18:31:52 +0530

    about ,favorites, notification change work
    about ,favorites, notification change work

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 
 import { createRoot } from 'react-dom/client';
+<<<<<<< Updated upstream
 
 
+=======
+import './stylesheet/style.css';
+import './stylesheet/Footer.css';
+import './stylesheet/Favorties.css'; 
+import './stylesheet/About.css'; 
+>>>>>>> Stashed changes
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
